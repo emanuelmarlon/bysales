0eaeba41a16fcb6c290051fb9015371a6e0cd2ee
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-02 18:52:01 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/actions/index.dart
+++ lib/custom_code/actions/index.dart
@@ -4,8 +4,5 @@ export 'request_notification_permissions.dart'
     show requestNotificationPermissions;
 export 'pararalarme_copy.dart' show pararalarmeCopy;
 export 'local_notification.dart' show localNotification;
-export 'notificacao2.dart' show notificacao2;
 export 'schedule_notification.dart' show scheduleNotification;
-export 'ascender_tela.dart' show ascenderTela;
 export 'cancelar_notificacao.dart' show cancelarNotificacao;
-export 'notificacao.dart' show notificacao;
