ddd79c355760ca1e159a39b06d67341957483b8e
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-18 12:35:56 +0000

    Updating to latest FlutterFlow output.

--- lib/agendamentos/agendamentos_widget.dart
+++ lib/agendamentos/agendamentos_widget.dart
@@ -35,6 +35,7 @@ class _AgendamentosWidgetState extends State<AgendamentosWidget>
     SchedulerBinding.instance.addPostFrameCallback((_) async {
       _model.alarme = await actions.redirecionarPagina();
       if (_model.alarme == true) {
+        await actions.sobrepor();
         await actions.ascenderTela();
         await actions.fullEscreen();
         await showModalBottomSheet(
